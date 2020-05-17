1345178f6ca7e13e0764a7fa6bc94fba1f682dd8
naman0301 <namanshah03012000@gmail.com>
2020-05-17 13:37:22 -0400

    WEBSITE

---
+++ web.config.js
@@ -0,0 +1,7 @@
+
+<staticContent>
+  <remove fileExtension=".woff" >
+  <mimeMap fileExtension=".woff" mimeType="application/x-font-woff" >
+  <remove fileExtension=".woff2" >
+  <mimeMap fileExtension=".woff2" mimeType="application/font-woff2" >
+</staticContent>
